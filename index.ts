f3546c3a3d94b4d9a06b782d1845b2e4db63f87d
dengyi1992 <dengyi1992@users.noreply.github.com>
2022-08-12 09:05:20 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,22 +1,25 @@
 interface City {
   center: google.maps.LatLngLiteral;
   population: number;
+  color: string;
 }
 
 const citymap: Record<string, City> = {
-  1: { center: { lat: 28.993609, lng: 113.344639 }, population: 1 },
-  2: { center: { lat: 28.993994, lng: 113.344885 }, population: 1 },
-  3: { center: { lat: 28.994154, lng: 113.345196 }, population: 1 },
-  4: { center: { lat: 28.994341, lng: 113.3454 }, population: 1 },
-  5: { center: { lat: 28.994889, lng: 113.346582 }, population: 1 },
-  6: { center: { lat: 28.994745, lng: 113.347057 }, population: 1 },
-  7: { center: { lat: 28.99482, lng: 113.347615 }, population: 1 },
-  8: { center: { lat: 28.994858, lng: 113.347926 }, population: 1 },
-  9: { center: { lat: 28.994998, lng: 113.34813 }, population: 1 },
-  10: { center: { lat: 28.995148, lng: 113.348409 }, population: 1 },
-  11: { center: { lat: 28.995252, lng: 113.348774 }, population: 1 },
-  12: { center: { lat: 28.995421, lng: 113.34916 }, population: 1 },
-  13: { center: { lat: 28.995468, lng: 113.349353 }, population: 1 },
+  1: {center: {lat:28.998374489,lng:113.348871901},    population: 1,    color: '#ff0000',  },
+  2: {center: {lat:28.997535837,lng:113.350816688},    population: 1,    color: '#ff0099',  },
+  3: {center: {lat:28.997549632,lng:113.353679721},    population: 1,    color: '#ccff00',  },
+  4: {center: {lat:28.994927188,lng:113.352413306},    population: 1,    color: '#cc6600',  },
+  5: {center: {lat:28.995651804,lng:113.351136316},    population: 1,    color: '#cc0000',  },
+  6: {center: {lat:28.996097184,lng:113.349953412},    population: 1,    color: '#cc3399',  },
+  7: {center: {lat:28.995972949,lng:113.348517521},    population: 1,    color: '#993300',  },
+  8: {center: {lat:28.995543458,lng:113.347391408},    population: 1,    color: '#993399',  },
+  9: {center: {lat:28.995234331,lng:113.346900781},    population: 1,    color: '#66cc00',  },
+  10: {center: {lat:28.996082363,lng:113.346895451},    population: 1,    color: '#33ff00',  },
+  11: {center: {lat:28.997398415,lng:113.348518846},    population: 1,    color: '#ccff00',  },
+
+  // 11: { center: { lat: 28.995252, lng: 113.348774 }, population: 1, color: '#333300', },
+  // 12: { center: { lat: 28.995421, lng: 113.34916 }, population: 1, color: '#0099cc', },
+  // 13: { center: { lat: 28.995468, lng: 113.349353 }, population: 1, color: '#3300ff', },
 };
 
 function initMap(): void {
@@ -27,6 +30,10 @@ function initMap(): void {
       zoom: 18,
       center: { lat: 28.9937899, lng: 113.3472627 },
       mapTypeId: 'satellite',
+      mapTypeControl: true,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+      }
     }
   );
 
@@ -35,10 +42,10 @@ function initMap(): void {
   for (const city in citymap) {
     // Add the circle for this city to the map.
     const cityCircle = new google.maps.Circle({
-      strokeColor: '#03DAC5',
+      strokeColor: citymap[city].color,
       strokeOpacity: 0.2,
       strokeWeight: 2,
-      fillColor: '#03DAC5',
+      fillColor: citymap[city].color,
       fillOpacity: 0.05,
       map,
       center: citymap[city].center,
@@ -47,9 +54,33 @@ function initMap(): void {
     new google.maps.Marker({
       position: citymap[city].center,
       map,
-      title: '-',
+      title: city + '-' + JSON.stringify(citymap[city].center),
+      label: `${city}`,
     });
   }
+  const flightPlanCoordinates = [
+    {lat:28.998374489,lng:113.348871901},
+    {lat:28.997535837,lng:113.350816688},
+    {lat:28.997549632,lng:113.353679721},
+    {lat:28.994927188,lng:113.352413306},
+    {lat:28.995651804,lng:113.351136316},
+    {lat:28.996097184,lng:113.349953412},
+    {lat:28.995972949,lng:113.348517521},
+    {lat:28.995543458,lng:113.347391408},
+    {lat:28.995234331,lng:113.346900781},
+    {lat:28.996082363,lng:113.346895451},
+    {lat:28.997398415,lng:113.348518846},
+    {lat:28.998374489,lng:113.348871901},
+  ];
+  const flightPath = new google.maps.Polyline({
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+  });
+
+  flightPath.setMap(map);
 }
 
 declare global {
@@ -59,3 +90,4 @@ declare global {
 }
 window.initMap = initMap;
 export {};
+//北京54
