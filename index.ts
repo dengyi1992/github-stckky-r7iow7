061c8e087075293a17cdcf6f04bfa4b30371e646
dengyi1992 <dengyi1992@users.noreply.github.com>
2022-08-15 15:26:20 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -5,18 +5,18 @@ interface City {
 }
 
 const citymap: Record<string, City> = {
-    1: {center:     {lat:28.998875761, lng:113.348858114},   population: 1,    color: '#ff0000',  },
-    2: {center:     {lat:28.998037095, lng:113.350802943},   population: 1,    color: '#ff0099',  },
-    3: {center:     {lat:28.998050891, lng:113.353666037},   population: 1,    color: '#ccff00',  },
-    4: {center:     {lat:28.995428401, lng:113.352399595},   population: 1,    color: '#cc6600',  },
-    5: {center:     {lat:28.996153030, lng:113.351122578},   population: 1,    color: '#cc0000',  },
-    6: {center:     {lat:28.996598417, lng:113.349939649},   population: 1,    color: '#cc3399',  },
-    7: {center:     {lat:28.996474179, lng:113.348503728},   population: 1,    color: '#993300',  },
-    8: {center:     {lat:28.996044681, lng:113.347377591},   population: 1,    color: '#993399',  },
-    9: {center:     {lat:28.995735549, lng:113.346886953},   population: 1,    color: '#66cc00',  },
-    10: {center:    {lat:28.996583595, lng:113.346881623},   population: 1,    color: '#33ff00',  },
-    11: {center:    {lat:28.997899669, lng:113.348505052},   population: 1,    color: '#ccff00',  },
-    "4+": {center:  {lat:28.9957907155,lng:113.3517610865},  population: 1,    color: '#cc6600',  },
+    1: {center:     {lat:28.998374489,lng:113.348871901}, population: 1,    color: '#ff0000',  },
+    2: {center:     {lat:28.997535837,lng:113.350816688}, population: 1,    color: '#ff0099',  },
+    3: {center:     {lat:28.997549632,lng:113.353679721}, population: 1,    color: '#ccff00',  },
+    4: {center:     {lat:28.994927188,lng:113.352413306}, population: 1,    color: '#cc6600',  },
+    5: {center:     {lat:28.995651804,lng:113.351136316}, population: 1,    color: '#cc0000',  },
+    6: {center:     {lat:28.996097184,lng:113.349953412}, population: 1,    color: '#cc3399',  },
+    7: {center:     {lat:28.995972949,lng:113.348517521}, population: 1,    color: '#993300',  },
+    8: {center:     {lat:28.995543458,lng:113.347391408}, population: 1,    color: '#993399',  },
+    9: {center:     {lat:28.995234331,lng:113.346900781}, population: 1,    color: '#66cc00',  },
+    10: {center:    {lat:28.996082363,lng:113.346895451}, population: 1,    color: '#33ff00',  },
+    11: {center:    {lat:28.997398415,lng:113.348518846}, population: 1,    color: '#ccff00',  },
+    "4+": {center:  {lat:28.995289496,lng:113.351774811},  population: 1,    color: '#cc6600',  },
 };
 
 function initMap(): void {
@@ -56,18 +56,18 @@ function initMap(): void {
     });
   }
   const flightPlanCoordinates = [
-    {lat:28.998875761, lng:113.348858114},
-    {lat:28.998037095, lng:113.350802943},
-    {lat:28.998050891, lng:113.353666037},
-    {lat:28.995428401, lng:113.352399595},
-    {lat:28.996153030, lng:113.351122578},
-    {lat:28.996598417, lng:113.349939649},
-    {lat:28.996474179, lng:113.348503728},
-    {lat:28.996044681, lng:113.347377591},
-    {lat:28.995735549, lng:113.346886953},
-    {lat:28.996583595, lng:113.346881623},
-    {lat:28.997899669, lng:113.348505052},
-    {lat:28.998875761, lng:113.348858114},
+    {lat:28.998374489,lng:113.348871901},
+    {lat:28.997535837,lng:113.350816688},
+    {lat:28.997549632,lng:113.353679721},
+    {lat:28.994927188,lng:113.352413306},
+    {lat:28.995651804,lng:113.351136316},
+    {lat:28.996097184,lng:113.349953412},
+    {lat:28.995972949,lng:113.348517521},
+    {lat:28.995543458,lng:113.347391408},
+    {lat:28.995234331,lng:113.346900781},
+    {lat:28.996082363,lng:113.346895451},
+    {lat:28.997398415,lng:113.348518846},
+    {lat:28.998374489,lng:113.348871901},
   ];
   const flightPath = new google.maps.Polyline({
     path: flightPlanCoordinates,
